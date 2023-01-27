5b534dda75654a25bb17e5626c7a5585eb325643
Partab Rai <amrani.partab@gmail.com>
2023-01-27 10:56:05 +0500

    changes

---
+++ src/components/Navbar/NavLink.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { page } from '$app/stores';
+  export let title;
+  export let href;
+</script>
+
+<li>
+  <a  href={href} class="link" class:active={$page.url.pathname == href}>
+    {title}
+  </a>  
+</li>
--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -1,13 +1,31 @@
 <script>
   import "./navbar.scss";
+	import NavLink from "./NavLink.svelte";
+  let nav = false;
 </script>
 
 <nav class="navbar">
   <ul>
-    <li><a href="/">QuotesProject</a></li>
+    <li><a href="/">Quotic</a></li>
   </ul>
-  <ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/about">About</a></li>
+  <div on:click={() => nav=true} class="menu">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+    </svg>
+  </div>
+  <ul class={`sidenav ${nav && "active"}`}>
+    <a class="close" on:click={() => nav = !nav} href="#">&times;</a>
+    <li>
+      <NavLink
+      title="Home"
+      href="/"
+      />
+    </li>
+    <li>
+      <NavLink
+      title="About"
+      href="/about"
+      />
+    </li>
   </ul>
 </nav>
