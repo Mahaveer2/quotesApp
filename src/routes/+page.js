66417479cc384a95c852529e74958a34f0cae840
Partab Rai <amrani.partab@gmail.com>
2023-02-05 13:32:43 +0500

    enhanced algorithm

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,7 +1,9 @@
 import { getQuotes } from "$lib/functions";
+import _, { map } from 'underscore';
 
 export async function load({ params }) {
   let data = await getQuotes()
+  data = _.shuffle(data);
   
   return {
     data:data,
