9e79f7ca3709d4f88a3c46e941b749f8fa4475f6
Partab Rai <amrani.partab@gmail.com>
2023-02-07 00:04:15 +0500

    phew

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,9 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-
 	kit: {
-		adapter: adapter({
-			split: true
-		})
+		adapter: adapter()
 	}
 };
 
