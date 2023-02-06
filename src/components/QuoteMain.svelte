ca08fe316b9e3912ac28468f5d6a7f17124422df
Partab Rai <amrani.partab@gmail.com>
2023-02-06 23:54:44 +0500

    changes

---
+++ src/components/QuoteMain.svelte
@@ -0,0 +1,20 @@
+<script>
+  let height = "";
+  export let quotes;
+  import Quote from "./Quote/Quote.svelte";
+</script>
+
+<div style="height:100vh" class="card-container">
+	<div class="card-float" bind:clientHeight={height} style={`--height:${height}px`} >
+		{#each quotes as quote}
+			<div
+				style="width:100%;display:flex;
+						justify-content:center;align-items:center;"
+			>
+				<a style="text-decoration: none;width:80%" href={`/quote/${quote.slug.current}`}>
+					<Quote isFull={false} {quote} />
+				</a>
+			</div>
+		{/each}
+	</div>
+</div>
--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,13 +1,17 @@
-import { getBackgrounds,getSiteSettings } from "$lib/functions";
+import { getBackgrounds,getQuotes,getSiteSettings } from "$lib/functions";
 import { settingData } from "$lib/stores.js";
+import _, { map } from 'underscore';
 
 export async function load({ params }) {
   let data = await getBackgrounds()
   let settings = await getSiteSettings();
   settingData.set(settings);
+  let quotes = await getQuotes()
+  quotes = _.shuffle(quotes);
   
   return {
     settings:settings,
     data:data,
+    quotes:quotes,
   }
 }
