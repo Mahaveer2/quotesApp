86b4501633b45480f804d0aa75d5008186d0d55b
Partab Rai <amrani.partab@gmail.com>
2023-02-10 16:14:59 +0500

    changes

--- src/routes/gallery/+page.server.js
+++ src/routes/gallery/+page.server.js
@@ -4,7 +4,8 @@ export async function load(){
   const client = new PrismaClient();
   let text = '';
   let data = await client.quote.findMany({
-    orderBy:[{views:'desc'}]
+    orderBy:[{views:'desc'}],
+    take:50
   })
 
   data.forEach(e => {
