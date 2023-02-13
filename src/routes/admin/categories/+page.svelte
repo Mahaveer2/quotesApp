e149445c39913c1de6a694bc5dcd91f77fa09043
Partab Rai <amrani.partab@gmail.com>
2023-02-13 10:06:47 +0500

    changes

--- src/routes/admin/categories/+page.svelte
+++ src/routes/admin/categories/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Fuse from 'fuse.js';
   export let data;
   const { categories } = data;
   import { goto } from "$app/navigation";
@@ -17,11 +18,35 @@
     goto("../")
     }
   }
+
+  let _categories = categories;
+
+  let search = (search) => {
+		const options = {
+			keys: ['title']
+		};
+
+		const fuse = new Fuse(categories, options);
+
+		if (search == '') {
+			_categories = categories;
+      _categories = _categories;
+		} else {
+			let res = fuse.search(search);
+      _categories = [];
+      console.log(res)
+			res.forEach((e) => {
+				_categories.push(e.item);
+				_categories = _categories;
+			});
+		}
+	}; 
 </script>
 
 <h1>Manage Categories</h1>
-
-{#each categories as category }
+<input type="text" on:keyup={(e) => search(e.target.value)} name="" placeholder="Search Categories">
+<div class="grid-1">
+{#each _categories as category }
   <div class="category" style="">
     <div>
     <strong>{category.title}</strong>
@@ -33,15 +58,29 @@
   >
   </div>
 {/each}
+</div>
 
 <style>
   .category{
 background-color: rgba(0,0,0,.3);
 padding: 20px;
+border: 1px solid #3c3c3c;
 margin-bottom: 20px;
-border-radius: 20px;
+border-radius: 8px;
 display: flex;
-justify-content: space-around;
+justify-content: space-between;
 align-items: center;
+  }
+  .grid-1{
+    display: grid;
+    grid-template-columns: repeat(3,1fr);
+    gap: 20px;
+  }
+  @media screen and (max-width:768px){
+    .grid-1{
+    display: grid;
+    grid-template-columns: repeat(1,1fr);
+    gap: 0px;
+  } 
   }
 </style>
