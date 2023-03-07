191b2e6d4054ef1d359c6e7edb0cd0cdf5126768
Partab Rai <amrani.partab@gmail.com>
2023-03-07 23:41:06 +0500

    THIS IS A CHANGE

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -7,7 +7,7 @@
   const { quotes } = data;
 </script>
 
-<div class="wrapper">
+<div class="wrapper" style={`--count_g:${quotes.length}`}>
   <div class="wrapper__container">
     {#each _.shuffle(quotes) as quote}
       <a href={`/quote/${quote.id}`} style={`--font-size:${Math.floor(Math.random() * 50 + 20)}px`}>{quote.quote}</a>
@@ -39,7 +39,7 @@
     display: flex;
     position: absolute;
     width: 100vw;
-    height: 100vh;
+    min-height: 100vh;
 		left:0;
 		flex-direction: column;
     top: 80px;
@@ -49,11 +49,8 @@
   .wrapper__container {
     display: flex;
     flex-direction: row;
-    flex-wrap: nowrap;
-    animation: scroll 60s linear infinite;
+    animation: scroll calc(var(--count_g) * 8s) linear infinite;
     width: max-content;
-    margin-left: -50vw;
-    margin-right: -50vw;
   }
 
   .wrapper__container a {
@@ -66,7 +63,6 @@
     color: white;
     padding: 20px;
     border-radius: 10px;
-    transition: transform 0.2s ease-in-out;
   }
 
   .wrapper__container a:hover {
@@ -78,7 +74,7 @@
       transform: translateX(0);
     }
     100% {
-      transform: translateX(-50%);
+      transform: translateX(-100%);
     }
   }
 </style>
